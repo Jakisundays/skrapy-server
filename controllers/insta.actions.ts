3fe5c1b4ad9aba5c9fd4c20669a9e2256fb5e667
Jakisundays <jakisundays@gmail.com>
2023-12-06 16:16:30 -0300

    [Feature] Likes implemented and comments needs more work on the thread

--- controllers/insta.actions.ts
+++ controllers/insta.actions.ts
@@ -1,9 +1,7 @@
-import { User } from "../types";
+import { Comment, User } from "../types";
 import { instaInstance } from "../utils/instaInstance";
 
-export const getAllFollowers = async (
-  idOrUsernameOrUrl: string,
-) => {
+export const getAllFollowers = async (idOrUsernameOrUrl: string) => {
   try {
     let allUsers: User[] = [];
     let paginationToken: string | undefined = undefined;
@@ -20,7 +18,7 @@ export const getAllFollowers = async (
       const { items } = data.data;
 
       const filteredUsers: User[] = items
-        .filter((user: any) => !user.is_private)
+        // .filter((user: any) => !user.is_private)
         .map((user: any) => ({
           id: user.id,
           userName: user.username,
@@ -35,6 +33,113 @@ export const getAllFollowers = async (
     } while (paginationToken);
 
     return new Response(JSON.stringify(allUsers));
+  } catch (error) {
+    console.error({ error });
+    return new Response("Bad request", { status: 500 });
+  }
+};
+
+export const getAllFollowing = async (idOrUsernameOrUrl: string) => {
+  try {
+    let allUsers: User[] = [];
+    let paginationToken: string | undefined = undefined;
+
+    do {
+      const { data }: any = await instaInstance.get("/following", {
+        params: {
+          username_or_id_or_url: idOrUsernameOrUrl,
+          pagination_token: paginationToken,
+        },
+      });
+
+      const nextPageToken = data.pagination_token;
+      const { items } = data.data;
+
+      const filteredUsers: User[] = items
+        // .filter((user: any) => !user.is_private)
+        .map((user: any) => ({
+          id: user.id,
+          userName: user.username,
+          full_name: user.full_name,
+          profile_pic_url: user.profile_pic_url,
+          is_verified: user.is_verified,
+          is_private: user.is_private,
+        }));
+
+      allUsers = allUsers.concat(filteredUsers);
+      paginationToken = nextPageToken;
+    } while (paginationToken);
+
+    return new Response(JSON.stringify(allUsers));
+  } catch (error) {
+    console.error({ error });
+    return new Response("Bad request", { status: 500 });
+  }
+};
+
+export const getLikersOfPost = async (code_or_id_or_url: string) => {
+  try {
+    let allUsers: User[] = [];
+    let paginationToken: string | undefined = undefined;
+    do {
+      const { data }: any = await instaInstance.get("/likes", {
+        params: { code_or_id_or_url, pagination_token: paginationToken },
+      });
+      const nextPageToken = data.pagination_token;
+      const { items } = data.data;
+      const userList: User[] = items
+        // .filter((user: any) => !user.is_private)
+        .map((user: any) => ({
+          full_name: user.full_name,
+          id: user.id,
+          profile_pic_url: user.profile_pic_url,
+          is_verified: user.is_verified,
+          userName: user.username,
+          is_private: user.is_private,
+        }));
+      allUsers = allUsers.concat(userList);
+      paginationToken = nextPageToken;
+    } while (paginationToken);
+    console.log({ length: allUsers.length });
+    return new Response(JSON.stringify(allUsers));
+  } catch (error) {
+    console.error({ error });
+    return new Response("Bad request", { status: 500 });
+  }
+};
+
+export const getCommentsOnPost = async (code_or_id_or_url: string) => {
+  console.log({ code_or_id_or_url });
+  try {
+    let allComments: Comment[] = [];
+    let paginationToken: string | undefined = undefined;
+    let totalComments;
+    do {
+      const { data }: any = await instaInstance.get("/comments", {
+        params: { code_or_id_or_url, pagination_token: paginationToken },
+      });
+      const nextPageToken = data.pagination_token;
+      const { items, total } = data.data;
+      totalComments = total;
+      const comments: Comment[] = items.map((comment: any) => ({
+        id: comment.id,
+        child_comment_count: comment.child_comment_count,
+        like_count: comment.like_count,
+        created_at_utc: comment.created_at_utc,
+        text: comment.text,
+        user: {
+          full_name: comment.user.full_name,
+          userName: comment.user.username,
+          id: comment.user.id,
+          profile_pic_url: comment.user.profile_pic_url,
+          is_verified: comment.user.is_verified,
+          is_private: comment.user.is_private,
+        },
+      }));
+      allComments = allComments.concat(comments);
+      paginationToken = nextPageToken;
+    } while (paginationToken);
+    return new Response(JSON.stringify(allComments));
   } catch (error) {
     console.error({ error });
     return new Response("Bad request", { status: 500 });
--- routes/scraper.ts
+++ routes/scraper.ts
@@ -1,9 +1,23 @@
 import { Elysia } from "elysia";
-import { getAllFollowers } from "../controllers/insta.actions";
+import {
+  getAllFollowers,
+  getAllFollowing,
+  getCommentsOnPost,
+  getLikersOfPost,
+} from "../controllers/insta.actions";
 
-const scraperRoutes = new Elysia({ prefix: "/scraper" }).get(
-  "/followers/:idOrUsernameOrUrl",
-  ({ params: { idOrUsernameOrUrl } }) => getAllFollowers(idOrUsernameOrUrl)
-);
+const scraperRoutes = new Elysia({ prefix: "/api/scraper" })
+  .get("/followers/:idOrUsernameOrUrl", ({ params: { idOrUsernameOrUrl } }) =>
+    getAllFollowers(idOrUsernameOrUrl)
+  )
+  .get("/following/:idOrUsernameOrUrl", ({ params: { idOrUsernameOrUrl } }) =>
+    getAllFollowing(idOrUsernameOrUrl)
+  )
+  .get("/likes/:code_or_id_or_url", ({ params: { code_or_id_or_url } }) =>
+    getLikersOfPost(code_or_id_or_url)
+  )
+  .get("/comments/:code_or_id_or_url", ({ params: { code_or_id_or_url } }) =>
+    getCommentsOnPost(code_or_id_or_url)
+  );
 
 export default scraperRoutes;
